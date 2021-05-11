8781c4d612a803624e73811ac1fd6316fc751569
Liz Emerson <uknity@gmail.com>
2021-05-10 23:58:02 -0400

    added git ignore; resized icons; added manifest.webmanifest

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3050;
+const PORT = 4000;
 
 const app = express();
 
